2f0853d09f8e0875434025aff65d4c8ee436f44e
翟洪元 <hongyuan3@staff.weibo.com>
2023-05-12 15:02:02 +0800

    更新信息

---
+++ FYReader-dev/app/build/generated/source/buildConfig/debug/xyz/fycz/myreader/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package xyz.fycz.myreader;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "zhy.hongyuan.debug";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 265;
+  public static final String VERSION_NAME = "2.6.5-12cdca9";
+}
